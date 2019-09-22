ef7c4da00bb11dcac4067f6a0733823387ba0a9a
federodes <federodes@gmail.com>
2019-09-22 21:35:28 +0200

    adding prototypes

--- index.ts
+++ index.ts
@@ -1,15 +1,5 @@
 import { registerRootComponent } from 'expo';
-import { AppRegistry } from 'react-native';
-// import './App/prototypes';
+import './src/prototypes';
 import App from './src/App';
-
-// https://github.com/apollographql/apollo-client/issues/3236
-// Object.setPrototypeOf = Object.setPrototypeOf || function (obj, proto) {
-// // eslint-disable-next-line no-proto
-//   obj.__proto__ = proto;
-//   return obj;
-// };
-
-// AppRegistry.registerComponent('SportySpots', () => App);
 
 registerRootComponent(App);
