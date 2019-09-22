843b1e844c7ab82ff8ed69bfd7124cfbecbf76bc
federodes <federodes@gmail.com>
2019-09-22 22:12:09 +0200

    readin constants from manifest

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {
+  SPORTS,
+  ACTIVITY_STATUSES,
+  ATTENDEE_ACTIONS,
+  CITIES,
+} from './constants';
 
 const App = () => (
   <View style={styles.container}>
     <Text>Open up App.tsx to start working on your app! HELLOOOOO TIOT</Text>
+    <Text>{JSON.stringify(SPORTS)}</Text>
+    <Text>{JSON.stringify(ACTIVITY_STATUSES)}</Text>
+    <Text>{JSON.stringify(ATTENDEE_ACTIONS)}</Text>
+    <Text>{JSON.stringify(CITIES)}</Text>
   </View>
 );
 
